2ce778b7480d20ab36de0e8d71b76636ef734c13
Sreeja Paluvatla <spaluvatla1@uco.edu>
2024-02-01 14:50:01 -0600

    initial commit

---
+++ public/controller/firebase_core.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDlq9a8D1pcnePvneKHEUuv4avf2ZbmImc",
+    authDomain: "spaluvatla1-cmsc5373-webapp.firebaseapp.com",
+    projectId: "spaluvatla1-cmsc5373-webapp",
+    storageBucket: "spaluvatla1-cmsc5373-webapp.appspot.com",
+    messagingSenderId: "880302340369",
+    appId: "1:880302340369:web:43cfe1223a749ab165f4b0"
+  };
+
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig);
