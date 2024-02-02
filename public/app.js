e91f6faf3ca796171a00cdf42d417a6a63f78a45
Sreeja Paluvatla <spaluvatla1@uco.edu>
2024-02-02 13:52:25 -0600

    commit

--- public/app.js
+++ public/app.js
@@ -1,7 +1,9 @@
 import { onClickHomeMenu,onClickMenu2Menu } from "./controller/menueventhandlers.js";
+import { signinPageView } from "./view/signin_page.js";
 
 //menu button handler
 
 document.getElementById('menu-home').onclick=onClickHomeMenu;
 document.getElementById('menu-menu2').onclick=onClickMenu2Menu;
 
+signinPageView();
